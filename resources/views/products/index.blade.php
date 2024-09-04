7c15a6b794f8742c2fa6826d82cc07befbca6a8f
unknown <clapisbryan@gmail.com>
2024-09-04 21:49:28 +0800

    added all product

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,12 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Product</h1>
-    <div>index</div>
+    <div>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Product Name</th>
+                    <th>Qty</th>
+                    <th>Price</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($products as $product)
+                <tr>
+                    <td>{{$product->id}}</td>
+                    <td>{{$product->name}}</td>
+                    <td>{{$product->qty}}</td>
+                    <td>{{$product->price}}</td>
+                    <td>{{$product->description}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </body>
+
 </html>
